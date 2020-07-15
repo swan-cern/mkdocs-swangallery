4ce28fbb175e03c0a6300ad7663fbf0b78f87807
Diogo Castro <diogo.castro@cern.ch>
2020-07-15 12:01:55 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding = "utf-8") as data:
 
 setup(
     name="mkdocs-swangallery",
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/swan-cern/mkdocs-swangallery',
     license='BSD',
     description='Plugin for MkDocs to genereate a SWAN Gallery',
